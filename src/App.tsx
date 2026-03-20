3335cdca1cb0c2933676d82c236e20ec984ec4ce
Paromita Sengupta <paromitasengupta@Paromitas-MacBook-Pro.local>
2026-03-19 18:30:59 -0700

    initial portfolio setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div style={{ padding: "60px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "40px" }}>Hi, I'm Paromita 👋</h1>
+
+      <p style={{ fontSize: "20px", marginTop: "20px" }}>
+        Full-Stack Developer building Data & AI projects
+      </p>
+
+      <button
+        style={{
+          marginTop: "30px",
+          padding: "12px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+        }}
+      >
+        View My Work
+      </button>
+    </div>
+  );
+}
+
+export default App;
